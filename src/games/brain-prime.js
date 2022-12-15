4b21f1c9e245cee5b52888e24d2ffa1f7a05a674
Boris <borisalekseev95@gmail.com>
2022-12-15 17:57:05 +0200

    fix files

---
+++ src/games/brain-prime.js
@@ -0,0 +1,26 @@
+import gameEngine from '../index.js';
+
+const instruction = 'Answer "yes" if the number is prime. Otherwise answer "no".';
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  const sqrt = Math.sqrt(num);
+  for (let i = 2; i <= sqrt; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const round = () => {
+  const number = getRandomNumber(1, 100);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  const question = `${number}`;
+  return [question, correctAnswer];
+};
+
+const brainPrime = () => {
+  gameEngine(instruction, round);
+};
+
+export default brainPrime;
