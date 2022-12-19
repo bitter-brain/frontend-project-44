54682440735ff8f44e138d484c01005258496b6c
Boris <borisalekseev95@gmail.com>
2022-12-19 21:54:12 +0200

    fix brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,25 +1,24 @@
-import gameEngine from '../index.js';
+import startGame, { getRandomNumber } from '../index.js';
 
 const instruction = 'Find the greatest common divisor of given numbers.';
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
 const getGcd = (num1, num2) => {
-  if (num2 !== 0) {
-    const k = num1 % num2;
-    return getGcd(num2, k);
+  if (!num2) {
+    return num1;
   }
-  return num1;
+  return getGcd(num2, num1 % num2);
 };
 
-const round = () => {
-  const num1 = getRandomNumber(0, 100);
-  const num2 = getRandomNumber(0, 100);
+const generateRound = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
   const question = `${num1} ${num2}`;
   const correctAnswer = String(getGcd(num1, num2));
   return [question, correctAnswer];
 };
 
-const brainGcd = () => {
-  gameEngine(instruction, round);
+const startBrainGcd = () => {
+  startGame(instruction, generateRound);
 };
 
-export default brainGcd;
+export default startBrainGcd;
