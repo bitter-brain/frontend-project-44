0fc583255e032a8ffcb9f413644a203e97ce64cc
Boris <borisalekseev95@gmail.com>
2022-12-21 12:42:43 +0200

    fix brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -7,7 +7,7 @@ const calculate = (num1, num2, operator) => {
     case '+': return num1 + num2;
     case '-': return num1 - num2;
     case '*': return num1 * num2;
-    default: throw new Error('Unknown operator');
+    default: throw new Error(`Unknown operator: '${operator}'!`);
   }
 };
 
